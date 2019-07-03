755111efe00c54d3ae6f30295a9ff3fb96045830
simiss98 <simanskas.e@gmail.com>
2019-07-03 23:24:49 +0300

    second kata geresne

--- 2kata.js
+++ 2kata.js
@@ -1,14 +1,19 @@
 function countPositivesSumNegatives(input) {
-    if (input == null || input.length < 1) {
+    if (input === null || input.length < 1) {
         return [];
-    }
-    var array = [0, 0];
+    } else {
 
-    for(var n = 0; n < input.length; n++) {
-        if(input[n] <= 0) {
-            array[1] += input[n];
-      } else {
-            array[0] += 1;
-      }
+        var array = [0, 0];
+
+        for(var n = 0; n < input.length; n++) {
+            if(input[n] <= 0) {
+                array[1] += input[n];
+            } else {
+                array[0] += 1;
+            }
+        }   
     }
     return array;
+}
+
+countPositivesSumNegatives([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15])
